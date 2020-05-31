b44c33f96a2ccf59d61baca21f48d130ccf01f0c
SIMOHANNANI <mohamed.hanani2019@gmail.com>
2020-05-31 19:37:07 +0100

    First release

---
+++ app.py
@@ -0,0 +1,17 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+
+app = dash.Dash(__name__)
+colors = {
+    'background-color': 'blue',
+
+}
+app.layout = html.Div(
+    children=[
+        html.H2('This our project !'),
+
+    ], )
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
