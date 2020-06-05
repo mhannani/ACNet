90b28c05be2e10d3a70ac7a2c4feba703a1d0701
SIMOHANNANI <mohamed.hanani2019@gmail.com>
2020-06-05 22:49:16 +0100

    Adding navbar ,some other stuff as card and shifting between tabs ...

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # -Dash_aprioriAlgorithmeApplication
 Web application to gain association rules by using Apriori algorithme .
+
+<a href="https://acnet.herokuapp.com/">https://racnet.herokuapp.com/</a>
 <a href="url"><img src="screenshots/screenshot.png" align="center" ></a>
--- app.py
+++ app.py
@@ -1,73 +1,83 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import dash_bootstrap_components as dbc
+from dash.dependencies import Input, Output
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-
-server = app.server
-
+# external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+external_scripts = ['https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js',
+                    'https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js',
+                    ]
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.SKETCHY], external_scripts=external_scripts)
 app.layout = html.Div([
+#---------------------------------------------------Navbar--------------------------------------------------------------
+    html.Nav([
+        html.A([
+            'Logo'
+        ], href='https://acnet.herokuapp.com/', className='navbar-brand'),
+        html.Div([
+            html.Ul([
+                html.Li([
+                    html.A([
+                        'Home',
+                    ], className='nav-link')
+                ], className='nav-item'),
+                html.Li([
+                    html.A([
+                        'Feature',
+                    ], className='nav-link')
+                ], className='nav-item'),
+
+                html.Li([
+                    html.A([
+                        'About',
+                    ], className='nav-link')
+                ]),
+
+            ], className='navbar-nav mr-auto'),
+            html.Div([
+                html.A([
+                    html.Span([
+                        html.Img(src='assets/img/github.png', style={'height': '35px'}),
+                    ], className='float-right'),
+                ], target='_Blank', href='https://github.com/SIMOHANNANI/ACNet/')
+            ], className='form-inline my-2 my-lg-0'),
+        ], className='collapse navbar-collapse'),
+
+    ], className='mt-3 navbar navbar-expand-sm navbar-light bg-light'),
+# --------------------------------------------------------Tabs----------------------------------------------------------
     html.Div([
-        ## first row : ##
-        html.Div([
-            # Dropdown menu :
-            dcc.Dropdown(
-                options=[
-                    {'label': 'option_1', 'value': 'value_1'},
-                    {'label': 'option_2', 'value': 'value_2'},
-                    {'label': 'option_3', 'value': 'value_2'}
-                ],
-                value='value_3'  # The default selected value
-            )
-        ], className="four columns"),
-        html.Div([
-            ## Dropdown with multi-selction:
-            dcc.Dropdown(
-                options=[
-                    {'label': 'option_1', 'value': 'value_1'},
-                    {'label': 'option_2', 'value': 'value_2'},
-                    {'label': 'option_3', 'value': 'value_2'}
-                ],
-                multi=True,
-            )
-        ], className="four columns"),
-        html.Div([
-            ## Radioitems :
-            dcc.RadioItems(
-                options=[
-                    {'label':'Radio_1','value':'Radio_1'},
-                    {'label':'Radio_2','value':'Radio_2'},
-                    {'label':'Radio_3','value':'Radio_3'}
-                ],
-                value='Radio_1',
-            )
-        ], className="four columns")
-    ], className="row"),
-    html.Div([
-        ## second row : ##
-        html.Div([
-            ##Checkboxes :
-            dcc.Checklist(
-                options=[
-                    {'label':'checklist_1','value':'checklist_1'},
-                    {'label':'checklist_2','value':'checklist_2'},
-                    {'label':'checklist_3','value':'checklist_3'}
-                ],
-            )
-        ], className="four columns"),
-        html.Div([
-            ##Slider:
-            dcc.Slider(
-                min=0,
-                max=4,
-                marks={i:str(i/10.0) for i in range(0,5,1)},
-                value=4,
-            ),
-        ], className="four columns"),
-    ], className="row"),
-], className="container-xl")
+        dbc.Card([
+            dbc.CardHeader([
+                dbc.Tabs([
+                    dbc.Tab(label='getting the data', tab_id='getting_the_data'),
+                    dbc.Tab(label='viewing the data', tab_id='viewing_the_data',className='nav-item'),
+                    dbc.Tab(label='visualizing the data', tab_id='visualizing_the_data',className='nav-item'),
+                ], id='card_tabs', card=True, active_tab='getting_the_data',className='nav nav-pills nav-fill'),
+            ]),
+            dbc.CardBody([
+                # The content of each tab goes here !
+            ],id='card_body')
+
+        ], className='mt-5'),
+    ],className='ml-5 mr-5'),
+],className='ml-4 mr-4')
+
+getting_the_data = 'getting_the_data'
+viewing_the_data = 'viewing_the_data'
+visualizing_the_data = 'visualizing_the_data'
+@app.callback(
+    Output(component_id='card_body',component_property='children'),
+    [Input(component_id='card_tabs',component_property='active_tab')],
+
+)
+def show_tab_content(active_tab):
+    if active_tab == 'getting_the_data':
+        return getting_the_data
+    elif active_tab == 'viewing_the_data':
+        return viewing_the_data
+    else:
+        return visualizing_the_data
 
 if __name__ == '__main__':
     app.run_server(debug=True)
