387edfdb44e142c2fc37e7f945bc17b40092667b
SIMOHANNANI <mohamed.hanani2019@gmail.com>
2020-06-05 01:16:20 +0100

    testing heroku server

--- app.py
+++ app.py
@@ -1,29 +1,27 @@
-import os
-
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import dash_bootstrap_components as dbc
+app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app.layout = html.Div(id='div', children=[
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+    dbc.Alert("This is a primary alert", color="primary", id="center"),
+    html.Div(children='Dash is a web application framework for python'),
+    dcc.Graph(
+        id="graph example",
+        figure={
+            'data': [
+                {'x': [1, 2, 3], 'y': [6, 2, 1], 'type': 'bar', 'name': 'SF'},
+                {}
 
-server = app.server
+            ]
+        }
 
-app.layout = html.Div([
-    html.H2('Hello World'),
-    dcc.Dropdown(
-        id='dropdown',
-        options=[{'label': i, 'value': i} for i in ['LA', 'NYC', 'MTL']],
-        value='LA'
     ),
-    html.Div(id='display-value')
-])
+]
 
-@app.callback(dash.dependencies.Output('display-value', 'children'),
-              [dash.dependencies.Input('dropdown', 'value')])
-def display_value(value):
-    return 'You have selected "{}"'.format(value)
+                      )
 
 if __name__ == '__main__':
     app.run_server(debug=True)
